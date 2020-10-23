d895fb0051b0f7feca8e965dbbaf6c5f7080730d
Anita Dan <anita@Anitas-MacBook-Air.local>
2020-10-23 19:41:57 -0400

    Adding 5days forecast

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ $(document).ready(function () {
       console.log(response)
 
       // displaying info (temp, humidity etc), use jquery, append to HTML
-      var cityDiv = $("#city").text("Current city: " + response.name);
+      var cityDiv = $("#city").text(response.name);
       var tempDiv = $("#temp").text("Temperature: " + response.main.temp);
-      var weathDiv = $("#weather").text("Weather: " + response.weather[0].main);
+      var weathDiv = $("#weather").text("Weather: " + response.weather[0].icon);
       var humidDiv = $("#humidity").text("Humidity: " + response.main.humidity  + "%");
       var windDiv = $("#wind-speed").text("Wind-speed: " + response.wind.speed);
       var uvIndex = $("#UV-index").text("UV-Index: " + response)
@@ -44,7 +44,7 @@ $(document).ready(function () {
 
     // grabbing users input and storing it
     var cityInput = $("#search-box").val();
-    // cities.push(cityInput)
+   
     var fivedayURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + APIkey
 
     $.ajax({
@@ -58,36 +58,77 @@ $(document).ready(function () {
 
       var results = response.list;
       console.log(results);
-      for (var i = 0; i < 5; i++) {
+      // for (var i = 0; i < 5; i++) {
 
         // Creating and storing a div tag
-        var dayoneDiv = $("<div>");
+        var dayoneDiv = $("<div>")
+        var date = $("<p>").text(results[0].dt_txt);
+        var pTemp = $("<p>").text("Temp: " + results[0].main.temp);
+        var pWeath = $("<p>").text("Weather: " + results[0].weather[0].icon)
+        var pHumid = $("<p>").text("Humidity: " + results[0].main.humidity + "%")
+        dayoneDiv.append(date, pTemp, pWeath, pHumid)
+        $("#card-title1").append(dayoneDiv)
+        $(".card-header1").append(date)
 
-        //results[0].main.temp
-        //results[1].main.temp
-        //results[2].main.temp
-        //results[i]
+        var daytwoDiv = $("<div>")
+        var date = $("<p>").text(results[1].dt_txt);
+        var pTemp = $("<p>").text("Temp: " + results[1].main.temp);
+        var pWeath = $("<p>").text("Weather: " + results[1].weather[0].icon)
+        var pHumid = $("<p>").text("Humidity: " + results[1].main.humidity + "%")
+        daytwoDiv.append(date, pTemp,pWeath, pHumid)
+        $("#card-title2").append(daytwoDiv)
+
+        var daytriDiv = $("<div>")
+        var date = $("<p>").text(results[2].dt_txt);
+        var pTemp = $("<p>").text("Temp: " + results[2].main.temp);
+        var pWeath = $("<p>").text("Weather: " + results[2].weather[0].icon)
+        var pHumid = $("<p>").text("Humidity: " + results[2].main.humidity + "%")
+        daytriDiv.append(date, pTemp, pWeath, pHumid)
+        $("#card-title3").append(daytriDiv)
+
+        var dayfoDiv = $("<div>")
+        var date = $("<p>").text(results[3].dt_txt);
+        var pTemp = $("<p>").text("Temp: " + results[3].main.temp);
+        var pWeath = $("<p>").text("Weather: " + results[3].weather[0].icon)
+        var pHumid = $("<p>").text("Humidity: " + results[3].main.humidity + "%")
+        dayfoDiv.append(date, pTemp, pWeath, pHumid)
+        $("#card-title4").append(dayfoDiv)
+
+        var dayfiDiv = $("<div>")
+        var date = $("<p>").text(results[4].dt_txt);
+        var pTemp = $("<p>").text("Temp: " + results[4].main.temp);
+        var pWeath = $("<p>").text("Weather: " + results[4].weather[0].icon)
+        var pHumid = $("<p>").text("Humidity: " + results[4].main.humidity + "%")
+        dayfiDiv.append(date, pTemp, pWeath, pHumid)
+        $("#card-title5").append(dayfiDiv)
+
         
-        var p = $("<p>").text("Temp: " + results[i].main.temp);
-        dayoneDiv.append(p)
-
-        $("#five-day").append(dayoneDiv)
 
 
-      }
+      // }
 
-      // displaying info (temp, humidity etc), use jquery, append to HTML
-
-      // var fiveDays = [];
-      // for (var i = 0; i < 5; i++);
-
-      // var tempDiv = $("#card-title").html("Temperature: " + response.main.temp);
-      // var weathDiv = $("#weather").text("Weather: " + response.weather[0].main);
-      // var humidDiv = $("#humidity").text("Humidity: " + response.main.humidity);
-      // var windDiv = $("#wind-speed").text("Wind-speed: " + response.wind.speed);
-      // var uvIndex = $("#UV-index").text("UV-Index: " + reponse)
     })
   });
+
+var searchCity = [];
+
+for (var i = 0; i < searchCity.length; i++) {
+
+  cityInput.push(searchCity);
+  window.localStorage(searchCity);
+
+  $("#list-group").text(searchCity).push(searchCity)
+
+
+  
+
+
+
+
+}
+
+
+
 });
 
 
@@ -96,5 +137,9 @@ $(document).ready(function () {
 // save searches at local storage and display them in search bar
 
 // convert Kelvin to Farenheit
+
+// make icons
+
+// UV index and colors
 
 // make clear button
