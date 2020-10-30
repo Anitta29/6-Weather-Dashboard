e0cd09afaf3be96f4c8fc3173d691a586d2c9bbd
Anita Dan <anita@Anitas-MacBook-Air.local>
2020-10-30 13:02:25 -0400

    Created two separate functions

--- script.js
+++ script.js
@@ -12,12 +12,22 @@ $(document).ready(function () {
     event.preventDefault();
 
     // grabbing users input and storing it
+    // var cityInput = $("#search-box").val().trim();
+    currentAPI();
+    fiveDayAPI();
+  });
+
+  // run your function here
+
+  //searchfuncitonhere()
+  function currentAPI(queryURL) {
     var cityInput = $("#search-box").val().trim();
 
     citySearches.push(cityInput)
     console.log(citySearches)
 
     localStorage.setItem("citySearches", JSON.stringify(citySearches));
+
 
 
     // building query URL link
@@ -31,14 +41,13 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(response)
 
-      // getUVIndex(response.coord.lat, response.coord.lon)
+
       // displaying info (temp, humidity etc), use jquery, append to HTML
       var cityDiv = $("#city").text(response.name);
       var tempDiv = $("#temp").text("Temperature: " + response.main.temp + "F");
       var weathDiv = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
       var humidDiv = $("#humidity").text("Humidity: " + response.main.humidity + "%");
       var windDiv = $("#wind-speed").text("Wind-speed: " + response.wind.speed);
-      // var uvIndex = $("#UV-index").text("UV-Index: " + response)
       $("#weather").html(weathDiv)
 
 
@@ -46,15 +55,22 @@ $(document).ready(function () {
       getUVIndex(response.coord.lat, response.coord.lon)
     });
 
-  
-  });
+  }
+
+
+
 
   var fiveDays = [];
-
+  // function fiveDayAPI(fivedayURL) {
   // here goes another function for 5 day call
-  $(".search").on("click", function (event) {
-    event.preventDefault();
+  // $(".search").on("click", function (event) {
+  //   event.preventDefault();
 
+
+
+  // });
+
+  function fiveDayAPI(fivedayURL) {
     // grabbing users input and storing it
     var cityInput = $("#search-box").val().trim();
 
@@ -78,7 +94,6 @@ $(document).ready(function () {
         // variable for date and time string
         var dateTime = results[i].dt_txt
 
-        // console.log(dateTime)
         // if time string includes noon put entire object in five day array
         if (dateTime.includes("12:00:00")) {
           fiveDay.push(results[i])
@@ -87,56 +102,60 @@ $(document).ready(function () {
 
       console.log(fiveDay)
 
-      moment(fiveDay[0].dt_txt).format('MMMM Do YYYY')
+      for (var i = 0; i < fiveDay.length; i++) {
 
-      var dayoneDiv = $("<div>")
-      
-      var date = $("<p>").text(moment(fiveDay[0].dt_txt).format('MMMM Do YYYY'));
-      var pTemp = $("<p>").text("Temp: " + fiveDay[0].main.temp + "F");
-      var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + fiveDay[0].weather[0].icon + ".png")
-      var pHumid = $("<p>").text("Humidity: " + fiveDay[0].main.humidity + "%")
-      dayoneDiv.append(date, pTemp, pWeath, pHumid)
-      $("#card-title1").empty().prepend(dayoneDiv)
+        moment(fiveDay[i].dt_txt).format('MMMM Do YYYY')
 
-      var daytwoDiv = $("<div>")
-      
-      var date = $("<p>").text(moment(fiveDay[1].dt_txt).format('MMMM Do YYYY'));
-      var pTemp = $("<p>").text("Temp: " + fiveDay[1].main.temp + "F");
-      var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + fiveDay[1].weather[0].icon + ".png")
-      var pHumid = $("<p>").text("Humidity: " + fiveDay[1].main.humidity + "%")
-      daytwoDiv.append(date, pTemp, pWeath, pHumid)
-      $("#card-title2").empty().prepend(daytwoDiv)
+        var dayoneDiv = $("<div>")
 
-      var daytriDiv = $("<div>")
-      
-      var date = $("<p>").text(moment(fiveDay[2].dt_txt).format('MMMM Do YYYY'));
-      var pTemp = $("<p>").text("Temp: " + fiveDay[2].main.temp + "F");
-      var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + fiveDay[2].weather[0].icon + ".png")
-      var pHumid = $("<p>").text("Humidity: " + fiveDay[2].main.humidity + "%")
-      daytriDiv.append(date, pTemp, pWeath, pHumid)
-      $("#card-title3").empty().prepend(daytriDiv)
+        var date = $("<p>").text(moment(fiveDay[i].dt_txt).format('MMMM Do YYYY'));
+        var pTemp = $("<p>").text("Temp: " + fiveDay[i].main.temp + "F");
+        var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + fiveDay[i].weather[0].icon + ".png")
+        var pHumid = $("<p>").text("Humidity: " + fiveDay[i].main.humidity + "%")
+        dayoneDiv.append(date, pTemp, pWeath, pHumid)
+        $("#card-title" + [i]).empty().prepend(dayoneDiv)
 
-      var dayfoDiv = $("<div>")
-      
-      var date = $("<p>").text(moment(fiveDay[3].dt_txt).format('MMMM Do YYYY'));
-      var pTemp = $("<p>").text("Temp: " + fiveDay[3].main.temp + "F");
-      var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + fiveDay[3].weather[0].icon + ".png")
-      var pHumid = $("<p>").text("Humidity: " + fiveDay[3].main.humidity + "%")
-      dayfoDiv.append(date, pTemp, pWeath, pHumid)
-      $("#card-title4").empty().prepend(dayfoDiv)
+      }
 
-      var dayfiDiv = $("<div>")
-      
-      var date = $("<p>").text(moment(fiveDay[4].dt_txt).format('MMMM Do YYYY'));
-      var pTemp = $("<p>").text("Temp: " + fiveDay[4].main.temp + "F");
-      var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + fiveDay[4].weather[0].icon + ".png")
-      var pHumid = $("<p>").text("Humidity: " + fiveDay[4].main.humidity + "%")
-      dayfiDiv.append(date, pTemp, pWeath, pHumid)
-      $("#card-title5").empty().prepend(dayfiDiv)
+      // var daytwoDiv = $("<div>")
 
-    })
+      // var date = $("<p>").text(moment(fiveDay[1].dt_txt).format('MMMM Do YYYY'));
+      // var pTemp = $("<p>").text("Temp: " + fiveDay[1].main.temp + "F");
+      // var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + fiveDay[1].weather[0].icon + ".png")
+      // var pHumid = $("<p>").text("Humidity: " + fiveDay[1].main.humidity + "%")
+      // daytwoDiv.append(date, pTemp, pWeath, pHumid)
+      // $("#card-title2").empty().prepend(daytwoDiv)
 
-  });
+      // var daytriDiv = $("<div>")
+
+      // var date = $("<p>").text(moment(fiveDay[2].dt_txt).format('MMMM Do YYYY'));
+      // var pTemp = $("<p>").text("Temp: " + fiveDay[2].main.temp + "F");
+      // var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + fiveDay[2].weather[0].icon + ".png")
+      // var pHumid = $("<p>").text("Humidity: " + fiveDay[2].main.humidity + "%")
+      // daytriDiv.append(date, pTemp, pWeath, pHumid)
+      // $("#card-title3").empty().prepend(daytriDiv)
+
+      // var dayfoDiv = $("<div>")
+
+      // var date = $("<p>").text(moment(fiveDay[3].dt_txt).format('MMMM Do YYYY'));
+      // var pTemp = $("<p>").text("Temp: " + fiveDay[3].main.temp + "F");
+      // var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + fiveDay[3].weather[0].icon + ".png")
+      // var pHumid = $("<p>").text("Humidity: " + fiveDay[3].main.humidity + "%")
+      // dayfoDiv.append(date, pTemp, pWeath, pHumid)
+      // $("#card-title4").empty().prepend(dayfoDiv)
+
+      // var dayfiDiv = $("<div>")
+
+      // var date = $("<p>").text(moment(fiveDay[4].dt_txt).format('MMMM Do YYYY'));
+      // var pTemp = $("<p>").text("Temp: " + fiveDay[4].main.temp + "F");
+      // var pWeath = $("<img>").attr("src", "http://openweathermap.org/img/w/" + fiveDay[4].weather[0].icon + ".png")
+      // var pHumid = $("<p>").text("Humidity: " + fiveDay[4].main.humidity + "%")
+      // dayfiDiv.append(date, pTemp, pWeath, pHumid)
+      // $("#card-title5").empty().prepend(dayfiDiv)
+
+    });
+  }
+
 
   $("#clear").on("click", function (event) {
     event.preventDefault();
@@ -163,7 +182,7 @@ $(document).ready(function () {
     for (var i = 0; i < citySearches.length; i++) {
       var li = $("<li>");
       var aTag = $("<a>");
-  
+
       li.addClass("list-group-item");
       aTag.addClass("p-2").attr("href", "#").attr("id", citySearches[i])
       aTag.text(citySearches[i])
@@ -176,10 +195,8 @@ $(document).ready(function () {
   }
   searchHistory();
 
-  
+
   function getUVIndex(lat, lon) {
-    // var lat = response.coord.lat
-    // var lon = response.coord.lon
 
     var currentUV = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIkey}`;
 
@@ -191,22 +208,20 @@ $(document).ready(function () {
       console.log(response);
       var uvI = response.value;
       console.log(uvI)
-      
+
       $("#UV-index").text("UV Index: " + uvI);
     });
 
-  
   }
 
-  $(".list-group").on("click", "li", function(){
-var city = $(this).attr("data-name");
-searchHistory(city)
+  $(".list-group").on("click", "li", function () {
+    var city = $(this).attr("id");
+    currentAPI(queryURL);
+    fiveDayAPI(fivedayURL);
 
   })
- 
+
 
 });
 
 // UV index and colors
-
-// grad the value of attr and run another ajax call
